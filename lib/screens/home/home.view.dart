88f479e7502399ba8a5ba5c34949f96dd78ec9a7
matvin <matheusviniciusf@gmail.com>
2024-03-04 02:27:44 -0300

    feat: add logout functionality to homescreen

--- lib/screens/home/home.view.dart
+++ lib/screens/home/home.view.dart
@@ -1,11 +1,11 @@
-import 'package:flash_app/home/flash_card/flashcard.widget.dart';
-import 'package:flash_app/home/viewmodel/home.viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../repositories/gestures_repo/gestures.repo.dart';
-
+import '../login/viewmodel/login.viewmodel.dart';
+import 'flash_card/flashcard.widget.dart';
+import 'viewmodel/home.viewmodel.dart';
 
 class Home extends StatelessWidget {
   final HomeViewModel controller = GetIt.I.get<HomeViewModel>();
@@ -14,31 +14,52 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: (event) => gesturesRepository.startGesture(DateTime.now(), event.position),
-      onPointerMove: (event) => gesturesRepository.addPointToGesture(event.position),
-      onPointerUp: (event) => gesturesRepository.finishGesture(DateTime.now(), event.position),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Observer(builder: (context) {
-            return Stack(
-              children: [
-                FlashCard(
-                  frontText: controller.secondCard.frontText,
-                  backText: controller.secondCard.backText,
-                  isTopCard: false,
-                ),
-                FlashCard(
-                  frontText: controller.topCard.frontText,
-                  backText: controller.topCard.backText,
-                  isTopCard: true,
-                ),
-              ],
-            );
-          }),
+    return Stack(
+      children: [
+        Listener(
+          onPointerDown: (event) =>
+              gesturesRepository.startGesture(DateTime.now(), event.position),
+          onPointerMove: (event) =>
+              gesturesRepository.addPointToGesture(event.position),
+          onPointerUp: (event) =>
+              gesturesRepository.finishGesture(DateTime.now(), event.position),
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: SafeArea(
+              child: Observer(
+                builder: (context) {
+                  return Stack(
+                    children: [
+                      FlashCard(
+                        frontText: controller.secondCard.frontText,
+                        backText: controller.secondCard.backText,
+                        isTopCard: false,
+                      ),
+                      FlashCard(
+                        frontText: controller.topCard.frontText,
+                        backText: controller.topCard.backText,
+                        isTopCard: true,
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
         ),
-      ),
+              Positioned(
+                top: 16,
+                right: 24,
+                child: IconButton(
+                            onPressed: () {
+                final loginScreenViewmodel = GetIt.I.get<LoginViewmodel>();
+                loginScreenViewmodel
+                    .logout()
+                    .then((value) => Navigator.of(context).pop());
+                            },
+                            icon: const Icon(Icons.logout)),
+              ),
+      ],
     );
   }
 }
